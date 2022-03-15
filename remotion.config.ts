848be2d8fb8cdc1fffb854348ab5883aed49be4b
Kamigami55 <eason@easonchang.com>
2022-03-16 01:33:49 +0800

    Install tailwindcss@v2 & prettier-plugin-tailwindcss
    https://www.remotion.dev/docs/webpack#enable-tailwindcss-support
    https://github.com/tailwindlabs/prettier-plugin-tailwindcss

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,47 @@
-import {Config} from 'remotion';
+import { Config } from 'remotion'
 
-Config.Rendering.setImageFormat('jpeg');
-Config.Output.setOverwriteOutput(true);
+Config.Rendering.setImageFormat('jpeg')
+Config.Output.setOverwriteOutput(true)
+
+// https://www.remotion.dev/docs/webpack#enable-tailwindcss-support
+Config.Bundling.overrideWebpackConfig((currentConfiguration) => {
+  return {
+    ...currentConfiguration,
+    module: {
+      ...currentConfiguration.module,
+      rules: [
+        ...(currentConfiguration.module?.rules
+          ? currentConfiguration.module.rules
+          : []
+        ).filter((rule) => {
+          if (rule === '...') {
+            return false
+          }
+          if (rule.test?.toString().includes('.css')) {
+            return false
+          }
+          return true
+        }),
+        {
+          test: /\.css$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    'postcss-preset-env',
+                    'tailwindcss',
+                    'autoprefixer',
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import './style.css'
+
 import { registerRoot } from 'remotion'
 
 import { RemotionVideo } from './Video'
