4531419f4aebdfb92acac3d2de6c2d9cc2953b6d
Kamigami55 <eason@easonchang.com>
2022-03-22 02:00:16 +0800

    Improve list screen UI

--- src/components/ProductListProduct.tsx
+++ src/components/ProductListProduct.tsx
@@ -26,15 +26,19 @@ export const ProductListProduct = ({ product, transitionStart }) => {
       className="flex items-center justify-between gap-4"
       style={{ opacity: opacity, transform: `translateY(${translate}px)` }}
     >
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-3">
         <Rank rank={product.rank} />
-        <img src={product.thumbnail} alt="" className="h-12 w-12" />
+        <img src={product.thumbnail} alt="" className="h-14 w-14" />
         <div>
-          <h2 className="text-4xl">{product.name}</h2>
-          <h3 className="text-2xl leading-6">{product.tagline}</h3>
+          <h2 className="mb-1 text-4xl leading-8 text-gray-900 line-clamp-2">
+            {product.name}
+          </h2>
+          <h3 className="text-2xl leading-7 text-gray-700 line-clamp-2">
+            {product.tagline}
+          </h3>
         </div>
       </div>
-      <div className="flex flex-col rounded border-2 border-gray-200 py-1 px-2 text-center">
+      <div className="flex flex-col rounded border-2 border-gray-200 py-1 px-2 text-center text-gray-800">
         <p className="text-xl font-semibold">▲</p>
         <p className="text-xl font-semibold">{product.votesCount}</p>
       </div>
