1bfaa9d619bac72ac439d113a3077df8987fca2b
Kamigami55 <eason@easonchang.com>
2022-03-22 01:50:04 +0800

    Improve ProductDetail UI

--- src/components/ProductDetailProduct.tsx
+++ src/components/ProductDetailProduct.tsx
@@ -4,16 +4,8 @@ import { ImagesCarousel } from './ImagesCarousel'
 import { Rank } from './Rank'
 
 export const ProductDetailProduct = ({ product }) => {
-  const {
-    rank,
-    thumbnail,
-    name,
-    tagline,
-    topics,
-    description,
-    images,
-    votesCount,
-  } = product
+  const { rank, thumbnail, name, topics, description, images, votesCount } =
+    product
   const frame = useCurrentFrame()
 
   const opacity = interpolate(frame, [0, 15], [0, 1], {
@@ -28,18 +20,18 @@ export const ProductDetailProduct = ({ product }) => {
 
   return (
     <div style={{ opacity: opacity, transform: `translateY(${translate}px)` }}>
-      <div className="mb-2 flex items-center justify-between gap-4">
-        <div className="flex items-center gap-4">
-          <Rank rank={rank} />
-          <img src={thumbnail} alt="" className="h-12 w-12" />
+      <div className="mb-3 flex items-center justify-between gap-4">
+        <div className="flex items-center gap-4 overflow-hidden">
+          <img src={thumbnail} alt="" className="h-20 w-20" />
           <div>
-            <h2 className="text-4xl text-gray-900">{name}</h2>
-            <h3 className="mb-2 text-xl leading-6 text-gray-700">{tagline}</h3>
+            <h2 className="mb-2 text-4xl leading-8 text-gray-900 line-clamp-2">
+              {name}
+            </h2>
             <div className="flex items-center gap-1">
               {topics.map((topic) => (
                 <span
                   key={topic}
-                  className="box-border rounded-sm border border-gray-400 bg-gray-50 px-2 py-1 text-sm text-gray-500"
+                  className="box-border rounded border border-gray-400 bg-gray-50 px-2 py-1 text-sm text-gray-500"
                 >
                   {topic}
                 </span>
@@ -47,14 +39,18 @@ export const ProductDetailProduct = ({ product }) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col rounded border-2 border-gray-200 py-1 px-2 text-center">
-          <p className="text-xl font-semibold">▲</p>
-          <p className="text-xl font-semibold">{votesCount}</p>
+
+        <div className="flex flex-col items-end gap-2">
+          <Rank rank={rank} />
+          <div className="flex gap-2 rounded border border-[#db4200] bg-[#f64900] py-1 px-2 text-center text-white">
+            <p className="text-xl font-semibold">▲</p>
+            <p className="text-xl font-semibold">{votesCount}</p>
+          </div>
         </div>
       </div>
 
-      <div className="mb-2">
-        <p className="text-xl leading-6 text-gray-700 line-clamp-3">
+      <div className="mb-3">
+        <p className="text-2xl leading-7 text-gray-700 line-clamp-3">
           {description}
         </p>
       </div>
